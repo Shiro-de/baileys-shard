6f00f4c5b99b8e61c96d7ee20b1fe105497072ce
ZTRdiamond <fatahillahalmakarim226@gmail.com>
2025-10-06 20:28:32 +0700

    fix: wrong export class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-export * as ShardManager from "./Client/index";
-export * as ShardInfo from "./Utils/ShardInfo";
-export * as ShardError from "./Utils/Error";
-
 import { ShardManager } from "./Client/index";
 import { ShardInfo } from "./Utils/ShardInfo";
 import { ShardError } from "./Utils/Error";
+
+export {
+  ShardManager,
+  ShardInfo,
+  ShardError
+}
 
 export default {
   ShardManager,
